a93e91e27c7bfc57543e886f95461ceed2fcd655
thecallmeBilalAshiq <f223309@cfd.nu.edu.pk>
2026-08-08 14:43:09 +0500

    a

---
+++ main.tf
@@ -0,0 +1,107 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Find the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Find the latest Ubuntu 24.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "ubuntu-web-sg"
+  description = "Allow SSH and HTTPS"
+  vpc_id      = data.aws_vpc.default.id
+
+  # SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outgoing traffic
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ubuntu-web-sg"
+  }
+}
+
+# EC2
+resource "aws_instance" "my_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  # YOUR key pair
+  key_name = "rsa_key_devops"
+
+  # YOUR security group
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  root_block_device {
+    volume_size = 15
+    volume_type = "gp3"
+    encrypted   = true
+  }
+
+  tags = {
+    Name        = "my-terraform-server"
+    Environment = "dev"
+  }
+}
+
+# Show public IP
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
+}
+
+# Show public DNS
+output "public_dns" {
+  value = aws_instance.my_server.public_dns
+}
