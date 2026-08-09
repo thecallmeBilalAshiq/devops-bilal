fb1882e4e8caa5b746a9d237f5d178aabdb61c46
thecallmeBilalAshiq <f223309@cfd.nu.edu.pk>
2026-08-09 15:28:14 +0500

    Final commit aftercompletion

--- main.tf
+++ main.tf
@@ -57,6 +57,15 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Outgoing traffic
   egress {
     description = "Allow all outbound traffic"
